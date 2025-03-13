b5c09412d28efb1fa20b6492ed8c468624ea548f
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-13 17:28:43 +0000

    relocate structure of folders to track and indetify

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  # For signup/sign-in
+    path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name="index.html"), name='home'),
 ]

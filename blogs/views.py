3f934b561e48dcbb79cf985b23c57cd7daa2e517
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-19 08:18:29 +0000

    update views.py to get_project_or_404

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
-# Create your views here.
+
+def topic_list(request, topic):
+    posts = BlogPost.objects.filter(topic=topic)
+    return render(request, 'blogs/topic_list.html', {'posts': posts, 'topic': topic})
+
+def blog_detail(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'blogs/blog_detail.html', {'post': post})

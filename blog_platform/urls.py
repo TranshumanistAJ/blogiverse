669c54693cd633e868ad0bc28c4ca53df21e536c
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-19 08:28:49 +0000

    add topic_list for /blogs/<topic>/ (e.g., /blogs/Faith & Spirituality/

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from blogs.views import topic_list, blog_detail
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name="index.html"), name='home'),
-]
+    path('blogs/<str:topic>/', topic_list, name='topic_list'),
+    path('blogs/post/<int:post_id>/', blog_detail, name='blog_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

d7685919928b04146a0670404d4e22099ea2d78c
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-26 09:21:51 +0000

    set up a new table in the DB for forums to manage them dynamically

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Forum(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic = models.CharField(max_length=100)  
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='blog_photos/', blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)

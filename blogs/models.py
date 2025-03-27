4807e926284742e340db2645b4533e86ad8cd4f2
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-27 10:45:03 +0000

    update models.py and add default=1 to blogpost

--- blogs/models.py
+++ blogs/models.py
@@ -13,7 +13,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='blog_photos/', blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)

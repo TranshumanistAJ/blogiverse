86226eac5e8c2b2e487d985b0b9b450beb0c2187
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-27 15:26:42 +0000

    add home view to fetch all forums and render index.html with them

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from .models import BlogPost
+from .models import Forum, BlogPost
 
+def home(request):
+    forums = Forum.objects.all()
+    return render(request, 'index.html', {'forums': forums})
 
-def topic_list(request, topic):
-    posts = BlogPost.objects.filter(topic=topic)
-    return render(request, 'blogs/topic_list.html', {'posts': posts, 'topic': topic})
+def topic_list(request, forum_name):
+    forum = get_object_or_404(Forum, name=forum_name)
+    posts = BlogPost.objects.filter(forum=forum)
+    return render(request, 'blogs/topic_list.html', {'forum': forum, 'posts': posts})
 
 def blog_detail(request, post_id):
     post = get_object_or_404(BlogPost, id=post_id)

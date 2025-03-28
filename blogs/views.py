61cc0760cf61669cc1d6cc10ca672d5ee8f631a4
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-28 20:01:57 +0000

    make edit_post check if the user is the post’s author, pre-fills the form with the post’s data, and saves updates.

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect  # Added redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Forum, BlogPost
-from .forms import BlogPostForm  # Import the form
-from django.contrib.auth.decorators import login_required  # For login restriction
+from .forms import BlogPostForm
+from django.contrib.auth.decorators import login_required
 
-# Existing views
 def home(request):
     forums = Forum.objects.all()
     return render(request, 'index.html', {'forums': forums})
@@ -17,16 +16,30 @@ def blog_detail(request, post_id):
     post = get_object_or_404(BlogPost, id=post_id)
     return render(request, 'blogs/blog_detail.html', {'post': post})
 
-# New view for creating posts
-@login_required  # Ensure only logged-in users can create posts
+@login_required
 def create_post(request):
     if request.method == 'POST':
-        form = BlogPostForm(request.POST, request.FILES)  # Include FILES for photo upload
+        form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            blog_post = form.save(commit=False)  # Don’t save yet
-            blog_post.author = request.user  # Set the logged-in user as author
-            blog_post.save()  # Save the post
-            return redirect('topic_list', forum_name=blog_post.forum.name)  # Redirect to forum
+            blog_post = form.save(commit=False)
+            blog_post.author = request.user
+            blog_post.save()
+            return redirect('topic_list', forum_name=blog_post.forum.name)
     else:
-        form = BlogPostForm()  # Empty form for GET request
+        form = BlogPostForm()
     return render(request, 'blogs/create_post.html', {'form': form})
+
+# New view for editing posts
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.author != request.user:  # Only author can edit
+        return redirect('blog_detail', post_id=post.id)  # Redirect if not authorized
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_detail', post_id=post.id)
+    else:
+        form = BlogPostForm(instance=post)  # Pre-fill with existing post data
+    return render(request, 'blogs/edit_post.html', {'form': form, 'post': post})

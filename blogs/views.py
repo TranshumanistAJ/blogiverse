d06e0769567886680a97f6b783d059110ac48b41
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-28 20:15:47 +0000

    add a delete_post view to views.py

--- blogs/views.py
+++ blogs/views.py
@@ -29,17 +29,28 @@ def create_post(request):
         form = BlogPostForm()
     return render(request, 'blogs/create_post.html', {'form': form})
 
-# New view for editing posts
 @login_required
 def edit_post(request, post_id):
     post = get_object_or_404(BlogPost, id=post_id)
-    if post.author != request.user:  # Only author can edit
-        return redirect('blog_detail', post_id=post.id)  # Redirect if not authorized
+    if post.author != request.user:
+        return redirect('blog_detail', post_id=post.id)
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
             return redirect('blog_detail', post_id=post.id)
     else:
-        form = BlogPostForm(instance=post)  # Pre-fill with existing post data
+        form = BlogPostForm(instance=post)
     return render(request, 'blogs/edit_post.html', {'form': form, 'post': post})
+
+# New view for deleting posts
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.author != request.user:  # Only author can delete
+        return redirect('blog_detail', post_id=post.id)
+    if request.method == 'POST':  # Confirm deletion via POST
+        forum_name = post.forum.name  # Store forum name before deletion
+        post.delete()
+        return redirect('topic_list', forum_name=forum_name)
+    return render(request, 'blogs/delete_post.html', {'post': post})

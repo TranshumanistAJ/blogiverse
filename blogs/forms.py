179ff274346bedfdf7d881038549d0fffc993f43
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-03-28 20:32:02 +0000

    add a form for the Forum model in forms.py

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,12 +1,22 @@
 from django import forms
-from .models import BlogPost
+from .models import BlogPost, Forum  # Added Forum
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'content', 'forum', 'photo']  # Fields for users to fill
+        fields = ['title', 'content', 'forum', 'photo']
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Enter your post title'}),
             'content': forms.Textarea(attrs={'placeholder': 'Write your blog post here'}),
-            'forum': forms.Select(),  
+            'forum': forms.Select(),
+        }
+
+# New form for Forum
+class ForumForm(forms.ModelForm):
+    class Meta:
+        model = Forum
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter forum name'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Describe the forum'}),
         }

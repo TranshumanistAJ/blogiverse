8727e0eb10d75dc9c935c91fec3e90af20cb528b
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-03 06:42:13 +0000

    git add blogs/views.py git commit -m "Restore full views.py with index fix" git push heroku main

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Forum, BlogPost
-from .forms import BlogPostForm, ForumForm  # Added ForumForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import JsonResponse
+from .models import Forum, BlogPost, Comment
+from .forms import BlogPostForm
 
-def home(request):
+def index(request):
     forums = Forum.objects.all()
     return render(request, 'index.html', {'forums': forums})
 
@@ -21,23 +23,23 @@ def create_post(request):
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            blog_post = form.save(commit=False)
-            blog_post.author = request.user
-            blog_post.save()
-            return redirect('topic_list', forum_name=blog_post.forum.name)
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, "Post created successfully!")
+            return redirect('blog_detail', post_id=post.id)
     else:
         form = BlogPostForm()
     return render(request, 'blogs/create_post.html', {'form': form})
 
 @login_required
 def edit_post(request, post_id):
-    post = get_object_or_404(BlogPost, id=post_id)
-    if post.author != request.user:
-        return redirect('blog_detail', post_id=post.id)
+    post = get_object_or_404(BlogPost, id=post_id, author=request.user)
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
+            messages.success(request, "Post updated successfully!")
             return redirect('blog_detail', post_id=post.id)
     else:
         form = BlogPostForm(instance=post)
@@ -45,25 +47,30 @@ def edit_post(request, post_id):
 
 @login_required
 def delete_post(request, post_id):
-    post = get_object_or_404(BlogPost, id=post_id)
-    if post.author != request.user:
-        return redirect('blog_detail', post_id=post.id)
+    post = get_object_or_404(BlogPost, id=post_id, author=request.user)
     if request.method == 'POST':
-        forum_name = post.forum.name
         post.delete()
-        return redirect('topic_list', forum_name=forum_name)
+        messages.success(request, "Post deleted successfully!")
+        return redirect('topic_list', forum_name=post.forum.name)
     return render(request, 'blogs/delete_post.html', {'post': post})
 
+@login_required
+def like_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if request.user in post.likes.all():
+        post.likes.remove(request.user)
+        messages.success(request, "Like removed.")
+    else:
+        post.likes.add(request.user)
+        messages.success(request, "Post liked!")
+    return JsonResponse({'likes_count': post.likes.count()})
 
 @login_required
-def create_forum(request):
-    if not request.user.is_staff:  
-        return redirect('home')
+def add_comment(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
     if request.method == 'POST':
-        form = ForumForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = ForumForm()
-    return render(request, 'blogs/create_forum.html', {'form': form})
+        content = request.POST.get('content')
+        if content:
+            Comment.objects.create(post=post, author=request.user, content=content)
+            messages.success(request, "Comment added!")
+    return redirect('blog_detail', post_id=post_id)

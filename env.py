cb9f617b62d8f6c0d28cb97d36c1eea767070b88
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-07 17:04:45 +0000

    Add env.py with correct DATABASE_URL

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 import os
-os.environ.setdefault('DATABASE_URL', 'postgres://ua8h241m4bkkaf:p96840b8049f95f6cc7965f81e505ffff87db3ac79d849784889bcd1dbdc3584a@cek6h0n4ffe7ur.cluster-czz5s@kz4scl.eu-west-1.rds.amazonaws.com:5432/dfhgm2pa01f100')
+os.environ.setdefault('DATABASE_URL', 'postgres://ueqe7n0evtqe4o:p617dc033d6be792290f3e4470566c08384c1239021e2c959b0defc1184dfd786@c4ggdlb6n7njd4.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d9pimlkl3qn19i')

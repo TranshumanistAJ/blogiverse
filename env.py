c1629e34dacb353ab7fd5ccc9d6fd1c4549a86cc
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-07 16:40:32 +0000

    create envy.py & update settings.py and postgres

---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ.setdefault('DATABASE_URL', 'postgres://ua8h241m4bkkaf:p96840b8049f95f6cc7965f81e505ffff87db3ac79d849784889bcd1dbdc3584a@cek6h0n4ffe7ur.cluster-czz5s@kz4scl.eu-west-1.rds.amazonaws.com:5432/dfhgm2pa01f100')

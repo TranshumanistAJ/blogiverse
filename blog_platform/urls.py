549234121497313d2edb0ea35545947fe231ff5c
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-13 17:11:57 +0000

    git add blog_platform/urls.py blogs/urls.py git commit -m "Restore urls.py and update blogs/urls.py" git push heroku main

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('blogs.urls')),
+    path('', views.topic_list, name='home'),
+    path('<str:forum_name>/', views.topic_list, name='topic_list'),
+    path('post/<int:post_id>/', views.blog_detail, name='blog_detail'),
+    path('create_post/', views.create_post, name='create_post'),
+    path('create_forum/', views.create_forum, name='create_forum'),
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]

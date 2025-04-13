2ad84bd4215e2cf1fa805a0f9189ef7036bef85f
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-13 13:50:17 +0000

    git add blog_platform/urls.py blogs/urls.py git commit -m "Update URLs for allauth, Summernote, blog views" git push heroku main

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from blogs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
-    path('', views.home, name='home'),
-    path('blogs/create/', views.create_post, name='create_post'),           # Moved up
-    path('blogs/create-forum/', views.create_forum, name='create_forum'),   # Moved up
-    path('blogs/post/<int:post_id>/', views.blog_detail, name='blog_detail'),
-    path('blogs/post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-    path('blogs/post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
-    path('blogs/<str:forum_name>/', views.topic_list, name='topic_list'),  # Moved down
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('blogs.urls')),
+]

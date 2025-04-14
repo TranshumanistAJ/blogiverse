0b70630a972fe2697fba3a8285bfa453ee3135fb
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-04-14 05:18:18 +0000

    Fix urls.py and views.py imports

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,17 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from blogs import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', views.home, name='home'),
-    path('blogs/create/', views.create_post, name='create_post'),
-    path('blogs/create-forum/', views.create_forum, name='create_forum'),
-    path('blogs/post/<int:post_id>/', views.blog_detail, name='blog_detail'),
-    path('blogs/post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-    path('blogs/post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
-    path('blogs/<str:forum_name>/', views.topic_list, name='topic_list'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('create/', views.create_post, name='create_post'),
+    path('create-forum/', views.create_forum, name='create_forum'),
+    path('post/<int:post_id>/', views.blog_detail, name='blog_detail'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('<str:forum_name>/', views.topic_list, name='topic_list'),
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
+]

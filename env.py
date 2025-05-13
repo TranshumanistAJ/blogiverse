368b88c8a424cfeb3ef09233d363363d52c895ff
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-05-13 06:47:58 +0000

    test if local database works

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_pqrFQ5XMCDd1@ep-broad-snow-a2w2uaun.eu-central-1.aws.neon.tech/elm_cargo_lash_212563"
+os.environ["SECRET_KEY"] = "django-insecure-qo#_&@v22q(gh1rq8!0y*23#!pq9tliasondj&ojhl9yicmhoj"
+os.environ["DEVELOPMENT"] = "True"

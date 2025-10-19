6008f486d38931942d4611e83849b7c2fbfcf629
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-10-19 16:56:58 +0000

    correct the 3 blank lines in the urls.py file line 29

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
